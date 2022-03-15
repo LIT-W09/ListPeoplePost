9c1f6495fdd9c6eb1a5f95cc0024c617d40aa166
Avrumi Friedman <a.friedman07@gmail.com>
2022-03-14 22:01:17 -0400

    Add project files.

---
+++ ListPeoplePost.Web/wwwroot/js/add.js
@@ -0,0 +1,43 @@
+﻿$(() => {
+    let num = 1;
+
+    $("#add-rows").on('click', function () {
+        $("#ppl-rows").append(`<div class="row person-row" style='margin-bottom:10px;'>
+                <div class="col-md-3">
+                    <input class="form-control" type="text" name="people[${num}].firstname" placeholder="First Name"/>
+                </div>
+                <div class="col-md-3">
+                    <input class="form-control" type="text" name="people[${num}].lastname" placeholder="Last Name"/>
+                </div>
+                <div class="col-md-3">
+                    <input class="form-control" type="text" name="people[${num}].age" placeholder="Age"/>
+                </div>
+                <div class="col-md-3">
+                   <button type="button" class="btn btn-danger delete">Delete</button>
+                </div>
+            </div>`);
+        num++;
+    });
+
+    $('#ppl-rows').on('click', '.delete', function () {
+        const button = $(this);
+        const row = button.closest('.person-row');
+        row.remove();
+        
+        let counter = 0;
+        $(".person-row").each(function () {
+            const row = $(this);
+            const inputs = row.find('input');
+
+            inputs.each(function () {
+                const input = $(this);
+                const name = input.attr('name');
+                const indexOfDot = name.indexOf('.');
+                const nameAttr = name.substring(indexOfDot + 1);
+                input.attr('name', `people[${counter}].${nameAttr}`);
+            })
+            counter++;
+        });
+
+    });
+});
